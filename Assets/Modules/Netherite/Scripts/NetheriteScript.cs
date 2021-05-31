f11a578dcd56356d51e875b77a2a948bc0a96285
Emik03 <emik.lakunishok@gmail.com>
2021-05-31 12:34:46 +0200

    Revert "Added hexOS and Phosphorescence mission description parsers"
    This reverts commit 3d079a36cbb30691418cd02b18d59029180e3634.

--- Assets/Modules/Netherite/Scripts/NetheriteScript.cs
+++ Assets/Modules/Netherite/Scripts/NetheriteScript.cs
@@ -15,12 +15,11 @@ public class NetheriteScript : ModuleScript
     public Renderer ModuleRenderer;
     public Texture[] ModuleTextures;
 
+    private static int CurrentlySolvingId { get; set; }
+    private static int NetheriteId { get; set; }
+    private int NetheriteCount { get { return IsEditor ? 3 : Get<KMBombInfo>().GetSolvableModuleNames().Where(m => m == Module.Name).Count(); } }
     internal int Stage { get; set; }
 
-    private int CurrentlySolvingId { get; set; }
-    private int NetheriteId { get; set; }
-    private int NetheriteCount { get { return IsEditor ? 3 : Get<KMBombInfo>().GetSolvableModuleNames().Count(m => m == Module.Name); } }
-    
     private float? Voltage
     {
         get
@@ -46,8 +45,6 @@ public class NetheriteScript : ModuleScript
 
     private IEnumerable<int> Serial { get { return Get<KMBombInfo>().GetSerialNumberNumbers(); } }
     private IEnumerable<int> SerialWithSolves { get { return Serial.Prepend(NetheriteId); } }
-
-    private NetheriteScript[] AllNetherites { get { return GetComponentInParent<KMBomb>().GetComponentsInChildren<NetheriteScript>(); } }
 
     private void Start()
     {
@@ -106,11 +103,8 @@ public class NetheriteScript : ModuleScript
         // This solves the module.
         if (Stage >= Sequence.Length)
         {
-            AllNetherites.ForEach(n =>
-            {
-                n.NetheriteId++;
-                n.CurrentlySolvingId = 0;
-            });
+            NetheriteId++;
+            CurrentlySolvingId = default(int);
 
             // Makes the module disappear.
             ModuleRenderer.transform.localScale = Vector3.zero;
