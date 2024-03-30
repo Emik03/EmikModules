2c019ae7eb3a84da70f2cebc3f52578fabcfdc47
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2024-03-30 14:10:02 -0400

    More useless usings

--- Assets/Modules/The Octadecayotton/Scripts/Animate.cs
+++ Assets/Modules/The Octadecayotton/Scripts/Animate.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using System.Linq;
 using UnityEngine;
-using Rnd = UnityEngine.Random;
 
 namespace TheOctadecayotton
 {
